12662383075b98c6bcdd42ccbccd06a11fd89240
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 17:37:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,6 +20,11 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+    holiday_hash.each {|s, holiday|
+  holiday.each {|h, e| 
+    if h == :memorial_day
+      e << supply
+    end}}
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
