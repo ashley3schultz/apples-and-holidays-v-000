f650ed66d72c33b46f64bed4374b1ee093ea1a88
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 19:20:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     season = s.capitalize.to_s
     holiday = holi.join
-    item = arr.capitalize.to_s
+    item = a.capitalize
     
     binding.pry
     
