027c121986b87e37d57e185323a90645b941bc34
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 20:03:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,13 +42,16 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |sk, season_hash|
+      season = sk.capitalize.to_s
+    
   season_hash.each {|hk, item_array|
     h = hk.to_s.split("_")
     h_array = []
     h.each {|h_word| h_array << h_word.capitalize}
+      holiday = h_array.join(" ")
     
-    season = sk.capitalize.to_s
-    holiday = h_array.join(" ")
+    
+    
     items = item_array.join(", ")
     puts "#{season}:"
     #puts "  #{holiday}: #{items}"
