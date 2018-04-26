107a633313760b314952f66e83e74e27060f708b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 19:46:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,8 +50,7 @@ def all_supplies_in_holidays(holiday_hash)
     season = sk.capitalize.to_s
     holiday = h_array.join(" ")
     items = item_array.join(", ")
-    puts "#{season}:
-      #{holiday}: #{items}"}}
+    puts "  #{holiday}: #{items}"}}
 end
 
 def all_holidays_with_bbq(holiday_hash)
