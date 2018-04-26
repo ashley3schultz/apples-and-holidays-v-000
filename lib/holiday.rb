12e83933220dce187660ee8701d24fa09f980767
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 17:45:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday| 
-    puts season.join
+    season.join
   end
   
   # iterate through holiday_hash and print items such that your readout resembles:
